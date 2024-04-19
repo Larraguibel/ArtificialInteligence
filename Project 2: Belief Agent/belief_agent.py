62b9d9db3bbd73d4d4f4bde37579c50d181cb677
Diego Larraguibel <diego@larraguibelf.cl>
2024-04-19 15:42:59 +0200

    prioirity code

--- Project 2: Belief Agent/belief_agent.py
+++ Project 2: Belief Agent/belief_agent.py
@@ -4,9 +4,11 @@ from sympy import symbols
 class BeliefBase:
     def __init__(self):
         self.beliefs = set()
+        self.priorities = dict()
 
-    def expand(self, belief):
+    def expand(self, belief, priority=1):
         self.beliefs.add(belief)
+        self.assign_priority(belief, priority)
 
     def contract(self, belief):
         if belief in self.beliefs:
@@ -20,6 +22,12 @@ class BeliefBase:
 
     def make_consistent_beliefs(self):
         pass
+
+    def assign_priority(self, belief, priority=None):
+        if priority is None:
+            self.priorities[belief.__str__()] = len(self.beliefs)
+        else:
+            self.priorities[belief.__str__()] = priority
 
     def __str__(self):
         return "{" + ", ".join(map(str, self.beliefs)) + "}"

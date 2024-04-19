9b7751cef9be72e58df966443bfcf3a352f09b26
Diego Larraguibel <diego@larraguibelf.cl>
2024-04-19 16:09:25 +0200

    to_CNF function implementation

--- Project 2: Belief Agent/belief_agent.py
+++ Project 2: Belief Agent/belief_agent.py
@@ -28,6 +28,25 @@ class BeliefBase:
             self.priorities[belief.__str__()] = len(self.beliefs)
         else:
             self.priorities[belief.__str__()] = priority
+            
+    def to_CNF(self):
+        cnf_beliefs = set()
+        for belief in self.beliefs:
+            cnf_beliefs.add(self._convert_to_cnf(belief))
+        return And(*cnf_beliefs)
+
+    def _convert_to_cnf(self, belief):
+        if isinstance(belief, And):
+            return And(*(self._convert_to_cnf(arg) for arg in belief.args))
+        elif isinstance(belief, Or):
+            return Or(*(self._convert_to_cnf(arg) for arg in belief.args))
+        elif isinstance(belief, Not):
+            return Not(self._convert_to_cnf(belief.args[0]))
+        elif isinstance(belief, Implies):
+            return Or(Not(self._convert_to_cnf(belief.args[0])), self._convert_to_cnf(belief.args[1]))
+        else:
+            return belief
+            
 
     def __str__(self):
         return "{" + ", ".join(map(str, self.beliefs)) + "}"

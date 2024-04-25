b8f73cdbcc4ee29f0b6f9e597407c9a19b77ff17
Diego Larraguibel <diego@larraguibelf.cl>
2024-04-25 19:29:18 +0200

    all subs

--- Project 2: Belief Agent/utils.py
+++ Project 2: Belief Agent/utils.py
@@ -2,3 +2,9 @@ from itertools import combinations
 
 def generate_subsets(belief_base, n):
     return list(combinations(belief_base, n))
+
+def generate_all_subsets(belief_base):
+    subsets = []
+    for n in range(len(belief_base)):
+        subsets.append(*generate_subsets(belief_base, n))
+    return subsets
